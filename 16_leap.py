82a1ece509bc003a644542a83b57a20f3020dbc6
Amol Ambkar <47270240+amolambkar@users.noreply.github.com>
2021-05-02 16:26:25 +0000

    Add files via upload

---
+++ 16_leap.py
@@ -0,0 +1,16 @@
+# program to check
+# year is leap
+# or not
+
+year = int(input("Year :"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("this is leap year")
+        else:
+            print("not leap year")
+    else:
+        print("leap year")
+else:
+    print("not leap year")
