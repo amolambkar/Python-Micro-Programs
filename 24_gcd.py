95803ded666aad85ea3564794b7fa7b830c7f6c0
Amol Ambkar <47270240+amolambkar@users.noreply.github.com>
2021-05-03 17:18:17 +0000

    gcd

---
+++ 24_gcd.py
@@ -0,0 +1,17 @@
+# program to find
+# H.C.F or G.C.D 
+# of two numbers
+
+num1 = int(input("num1: "))
+num2 = int(input("num2: "))
+
+if num1 < num2:
+    smaller = num1
+else:
+    smaller = num2
+
+for i in range(1,smaller+1):
+    if (num1%i == 0) and (num2%i == 0):
+        gcd = i
+    
+print(gcd)
